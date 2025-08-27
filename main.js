60659268f1f5d1dbe433be409fd434dc179a2c9e
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-08-27 18:51:26 +0200

    add_div

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 // URL dell'endpoint
-const ApiUrl = "https://swapi.info/api/people"
+const ApiUrl = "https://swapi.info/api/people";
 
 // Contenitore del DOM
-let app = document.getElementById("app") 
+let app = document.getElementById("app");
+
+// Funzione per creare la card del personaggio
+function CreaCardPerson(personaggio) {
+
+    // contenitore div
+    let card = document.createElement("div");
+
+
+}
+
+
